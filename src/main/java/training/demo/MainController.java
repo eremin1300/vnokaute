4763574293e9484a3f83ce023aedadaf5e7faab7
eremin1300 <eremin1300@gmail.com>
2020-07-22 20:32:40 +0700

    v0.02.1

---
+++ src/main/java/training/demo/MainController.java
@@ -0,0 +1,46 @@
+package training.demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import training.demo.domain.message;
+import training.demo.repository.messageRepo;
+import java.util.Map;
+
+
+@Controller
+public class MainController {
+    @Autowired
+    private messageRepo messageRepo;
+
+    @GetMapping("/")
+    public String main(Map<String, Object> model) {
+        Iterable<message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        message messages = new message(text, tag);
+        messageRepo.save(messages);
+        model.put("messages", messages);
+        return "redirect:/";
+    }
+
+    @PostMapping ("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<message> messages;
+
+        if (filter !=null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }
+        else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+        return "main";
+    }
+
+}
